2e5fb66c47db3d6d68884060b21b321c47f6ad57
Twbreak <155842037+Twbreak@users.noreply.github.com>
2024-10-09 17:25:39 +0800

    update cnn_method2

--- model_training_process.py
+++ model_training_process.py
@@ -1,6 +1,11 @@
+import numpy as np
 from tensorflow import keras
 from keras import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
+
+from sklearn.preprocessing import StandardScaler
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 
 def get_cnn_model(input_shape, num_classes, learning_rate=0.001, num_filters=32, dense_units=256):
     cnn_model = Sequential()
@@ -58,3 +63,29 @@ def get_cnn_model(input_shape, num_classes, learning_rate=0.001, num_filters=32,
     )
 
     return cnn_model
+
+def get_processed_data(X, y, num_classes, mfcc_dim_1, mfcc_dim_2, channel, test_size):
+    scaler = StandardScaler()
+
+    # 因為標準器只能fit 2維的資料所以要將3維的mfcc資料集reshape成2維
+    # mfcc_data_nums: k
+    # mfcc_row: n
+    # mfcc_column: m
+    # (k, n, m) => (k * n, m)
+    scaler.fit(
+        X.reshape(
+            (X.shape[0] * X.shape[1], X.shape[2])
+        )
+    )
+
+    X_scaled = np.array([scaler.transform(data) for data in X])
+
+    # 將int label轉換成二進制one hot標籤
+    y_one_hot = to_categorical(y, num_classes=num_classes)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_one_hot, test_size=test_size, random_state=42)
+
+    X_train = X_train.reshape(X_train.shape[0], mfcc_dim_1, mfcc_dim_2, channel)
+    X_test = X_test.reshape(X_test.shape[0], mfcc_dim_1, mfcc_dim_2, channel)
+
+    return X_train, X_test, y_train, y_test
